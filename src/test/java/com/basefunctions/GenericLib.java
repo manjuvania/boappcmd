0d39aceda5b2c33d1a75fe764bb765ccb2934dfd
manjuvani.a <manjuvani.m3@gmail.com>
2017-08-28 23:40:44 +0530

    commiting

--- src/test/java/com/basefunctions/GenericLib.java
+++ src/test/java/com/basefunctions/GenericLib.java
@@ -1,15 +1,19 @@
 package com.basefunctions;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 
  
 public class GenericLib {
 	public static String configfilePath=BaseClass.sdirPath+"\\config.properties";
-	public static String excelpath=BaseClass.sdirPath+"\\AbofApplication.xlsx";
+	public static String excelpath=BaseClass.sdirPath+"\\boappdata.xlsx";
       
 	
 	public static String getProperties(String key){
@@ -35,11 +39,15 @@ public class GenericLib {
 		}
 	}
 	
-	public static String getExcelData(String sheetName,int row,int cell){
+	/*public static String getExcelData(String sheetName,int row,int cell) throws EncryptedDocumentException, InvalidFormatException, IOException{
 		String sdata=null;
 		FileInputStream file=new FileInputStream(excelpath);
-		Workbook wb=(Workbook)WorkbookFactory().create(file);
+		Workbook wb=(Workbook)new WorkbookFactory().create(file);
+		Sheet sh=wb.getSheet("logindata");
+		sdata=sh.getRow(row).getCell(cell).getStringCellValue();
+		return sdata;
 		
 		
-	}
+		
+	}*/
 }
